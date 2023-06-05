04cbf56d147b1277a776913f6a1b3f73a4a572cb
Houssem Saafi <69660781+HoussemSaafi@users.noreply.github.com>
2023-06-05 15:27:01 +0000

    filter selection working for sidebar/category

--- src/components/sidebar/Category.js
+++ src/components/sidebar/Category.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function Category() {
+export default function Category({setCategory}) {
+     const [quantities, setQuantities] = useState({})
+    const handleClick = () => {
+        console.log("hello");
+    }
+    //This funtion will be used to fetch the quantity of each category on component first mount.
+    useEffect(()=>{
+        setQuantities ({
+            menQuantity : 12,
+            womenQuantity : 15,
+            bagsQuantity : 13,
+            clothingQuantity : 16,
+            shoesQuantity : 18,
+            accessoiriesQuantity : 9,
+            kidsQuantity : 11
+        })
+    },[]);
+
   return (
     //category
     <div class="card">
@@ -11,15 +28,13 @@ export default function Category() {
             <div class="card-body">
                 <div class="shop__sidebar__categories">
                     <ul class="nice-scroll">
-                        <li><a href="#">Men (20)</a></li>
-                        <li><a href="#">Women (20)</a></li>
-                        <li><a href="#">Bags (20)</a></li>
-                        <li><a href="#">Clothing (20)</a></li>
-                        <li><a href="#">Shoes (20)</a></li>
-                        <li><a href="#">Accessories (20)</a></li>
-                        <li><a href="#">Kids (20)</a></li>
-                        <li><a href="#">Kids (20)</a></li>
-                        <li><a href="#">Kids (20)</a></li>
+                        <li><a className="clickable-element" onClick={()=>setCategory("Men")}>Men ({quantities?.menQuantity})</a></li>
+                    <li><a className="clickable-element" onClick={()=>setCategory("Women")}>Women ({quantities?.womenQuantity}) </a></li>
+                        <li><a className="clickable-element" onClick={()=>setCategory("Bags")}>Bags ({quantities?.bagsQuantity})</a></li>
+                        <li><a className="clickable-element" onClick={()=>setCategory("Clothing")}>Clothing ({quantities?.clothingQuantity})</a></li>
+                        <li><a className="clickable-element" onClick={()=>setCategory("Shoes")}>Shoes ({quantities?.shoesQuantity})</a></li>
+                        <li><a className="clickable-element" onClick={()=>setCategory("Accessoiries")}>Accessories ({quantities?.accessoiriesQuantity})</a></li>
+                        <li><a className="clickable-element" onClick={()=>setCategory("Kids")}>Kids ({quantities?.kidsQuantity})</a></li>
                     </ul>
                 </div>
             </div>
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './styles.css';
 import Category from './Category'
 import State from './State'
 import Price from './Price'
@@ -9,17 +10,29 @@ import Age from './Age'
 
 
 export default function Sidebar() {
+  const [category, setCategory] = useState("");
+  const [type, setType] = useState("");
+  const [race, setRace] = useState("");
+  const [price, setPrice] = useState("");
+  const [weight, setWeight] = useState("");
+  const [age, setAge] = useState("");
+  const [state, setState] = useState("");
+
+  useEffect(()=>{
+    console.log("category changed: ", category);
+  },[category,type,race,price,weight,age,state]);
+  
   return (
     <div>
         <div class="shop__sidebar__accordion">
             <div class="accordion" id="accordionExample">
-                <Category/>
-                <Type/>
-                <Race/>
-                <Price/>
-                <Weight/>
-                <Age/>
-                <State/>
+                <Category setCategory={setCategory}/>
+                <Type setType={setType}/>
+                <Race setRace={setRace}/>
+                <Price setPrice={setPrice}/> 
+                <Weight setWeight={setWeight}/>
+                <Age setAge={setAge}/>
+                <State state={setState}/>
             </div>
         </div>
     </div>
