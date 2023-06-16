d3c04c39132086bbfab4383fea3aa461673d91df
Houssem Saafi <houssemsaafi@insat.u-carthage.tn>
2023-06-16 02:29:43 +0000

    css fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,19 +38,19 @@ const Header = () => {
         <header className="header">
             <div className="container">
                 <div className="houssam">
-                    <div className="col-lg-3 col-md-3" style={{display:"flex"}}>
+                    <div className="col-lg-3 col-md-3" style={{ display: "flex" }}>
                         <div className="header__logo">
 
                             <Link to={routes.HOME}>
 
-                                <img src="./logo1.png" alt="Your Image"  />
+                                <img src="./logo1.png" alt="Your Image" />
                             </Link>
 
                         </div>
                     </div>
-                    <div className="col-lg-6 col-md-6" style={{display:"flex"}}>
+                    <div className="col-lg-5 col-md-5" style={{ display: "flex" }}>
                         <nav className="header__menu mobile-menu">
-                            <ul>
+                            <ul style={{padding:"16px 0"}}>
                                 <li>
                                     <Link to={routes.HOME}>
                                         {HeaderTranslations[language]["Acceuil"]}
@@ -69,8 +69,8 @@ const Header = () => {
                             </ul>
                         </nav>
                     </div>
-                    <div className="col-lg-3 col-md-3">
-                        <div className="header__nav__option">
+                    <div className="col-lg-4 col-md-4" style={{ display: "flex" }}>
+                        <div className="header__nav__option" style={{ display: "flex" }}>
                             <LanguageSelector />
                             <Link to={routes.CART}>
                                 <img src="img/icon/cart.png" alt="" /> <span></span>
@@ -117,6 +117,15 @@ const Header = () => {
                         >
                             {HeaderTranslations[language]["A Propos"]}
                         </Link>
+                    </li>
+                    <li>
+                        <LanguageSelector />
+                    </li>
+                    <li>
+                        <Link to={routes.CART}>
+                            <img src="img/icon/cart.png" alt="" /> <span></span>
+                        </Link>
+                        <div className="price">{cartItems.reduce((total, item) => total + item.price, 0)} {language === "fr" ? "Dinars" : "دينار"}</div>
                     </li>
                 </ul>
             </div>
