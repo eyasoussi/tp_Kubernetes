498ede8fb64a64c6ef318e992aaaa042b4b917ab
Eya Soussi <eyasoussi@insat.u-carthage.tn>
2023-06-16 04:31:24 +0000

    change of colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
                 <div className="col-lg-3 col-md-6 col-sm-6">
                     <div className="footer__about">
                         <div className="footer__logo">
-                            <a href="#"><img src="img/footer-logo.png" alt=""/></a>
+                            <a href="#"><img src="./logo1.png" alt=""/></a>
                         </div>
                         <p>The customer is at the heart of our unique business model, which includes design.</p>
                         <a href="#"><img src="img/payment.png" alt=""/></a>
