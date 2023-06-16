2d2ed3911062a561b5e2c269a9cf95549f533904
Houssem Saafi <houssemsaafi@insat.u-carthage.tn>
2023-06-16 03:26:41 +0000

    go back to the top fix

--- src/components/main-shop/Card.js
+++ src/components/main-shop/Card.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { useContext } from 'react';
 import { LanguageContext } from '../../LanguageContext';
+import { CartContext } from '../../CartContext';
+import routes from '../../routes';
+import { Link } from 'react-router-dom';
 
 export default function Card({item,key,handleShopItemClick}) {
     const { language } = useContext(LanguageContext);
+    const { addToCart } = useContext(CartContext);
+
+    const handleClick = () => {
+        addToCart(item);
+    }
   return (
     <div className="col-lg-4 col-md-6 col-sm-6"  onClick={() => handleShopItemClick(item.id)}>
                             <div className="product__item">
                                 <div className="product__item__pic set-bg" data-setbg={item.thumbnail}>
                                     <img src={item.thumbnail} alt=""></img>
                                     <ul className="product__hover">
-                                        <li><a><img src="img/icon/heart.png" alt=""/></a></li>
-                                        <li><a><img src="img/icon/compare.png" alt=""/> <span>{ language==="fr"? "Comparer": "مقارنة"}</span></a>
-                                        </li>
-                                        <li><a><img src="img/icon/search.png" alt=""/></a></li>
+                                        <li><Link onClick={handleClick} to={routes.CART} ><a><img src="img/icon/cart.png" alt=""/></a><span>{ language==="fr"? "Ajouter à la Carte": "أضف الى عربة المقتنيات"}</span></Link></li>
+                                        <li><Link to={`${routes.SHOPDETAILS}/${item.id}`}><a><img src="img/icon/search.png" alt=""/></a><span>{ language==="fr"? "Voir le Produit": "النضر الى المنتج"}</span></Link></li>
                                     </ul>
                                 </div>
                                 <div className="product__item__text">
