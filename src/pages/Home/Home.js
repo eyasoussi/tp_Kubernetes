10ab92e6239575b8186f74c8d944c3d13f2de2fe
falleh undefined <falleh.tn@gmail.com>
2023-06-24 03:10:30 +0000

    c

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -19,11 +19,14 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import GTranslateIcon from '@mui/icons-material/GTranslate';
 import SettingsIcon from '@mui/icons-material/Settings';
-import SimpleDialog from "../../components/SimpleDialog"
+import SimpleDialog from "../../components/SimpleDialog";
+import { Carousel } from 'react-responsive-carousel';
+import YouTube from 'react-youtube'
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function Home() {
     const myTabs = ["advice_alfa", "advice_elevage", "advice_vaccin"];
-    const { language, updateLanguage} = useContext(LanguageContext);
+    const { language, updateLanguage } = useContext(LanguageContext);
     const [activeFilter, setActiveFilter] = useState(myTabs[0]); // State to keep track of active filter
 
     const HomeTranslations = {
@@ -38,7 +41,7 @@ export default function Home() {
         "ar": {
             "Nouvel arrivage 2023": "تعرف على منتوجاتنا لسنة 2023",
             "Consultez Notre Shop": "أنقر هنا لترى منتوجاتنا",
-            "Achetez maintenant":"تصفح متجرنا",
+            "Achetez maintenant": "تصفح متجرنا",
             "Ovins & Brebis": "أراء حرفائنا",
             "Poulailler & Agnelles": "ثقتكم هي سر عزيمتنا لتحسين منتوجاتنا",
             "Contactez Nous": " اتصلوا بنا من أجل اجابة استفساراتكم",
@@ -110,12 +113,20 @@ export default function Home() {
             handleClickOpen();
         }
     }
-
+    const isMobileView = window.innerWidth < 768;
     const [isOpen, setIsOpen] = useState(false);
     const [selectedValue, setSelectedValue] = React.useState(language);
 
     const handleClickOpen = () => {
         setIsOpen(true);
+    };
+
+    const opts = {
+        height: isMobileView ? '375' : '840',
+        width: isMobileView ? '281' : '855',
+        playerVars: {
+            autoplay: 0,
+        },
     };
 
     const handleClickClose = (value) => {
@@ -131,12 +142,23 @@ export default function Home() {
             <Preloader />
             <Header />
             <section className="hero">
-                <AwesomeSlider>
+                <Carousel autoPlay={true} showArrows={true} interval={2000}>
+                    <div>
+                        <img src="https://scontent.ftun4-2.fna.fbcdn.net/v/t1.15752-9/256779859_213304307500755_4805841473852462713_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=101&ccb=1-7&_nc_sid=ae9488&_nc_ohc=PwGwkKQlBzQAX8PcMS1&_nc_ht=scontent.ftun4-2.fna&oh=03_AdTey-4_3YWe_8inhAV6glbteLoxJc0jlW09YIkwEVziqw&oe=64B37421" style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt="Image 1" />
+                    </div>
+                    <div>
+                        <YouTube videoId="_bJeu1nHVwU" opts={opts} />
+                    </div>
+                    <div>
+                        <img src="https://scontent.ftun4-2.fna.fbcdn.net/v/t1.15752-9/354671672_250588797603709_4163798981858942408_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=111&ccb=1-7&_nc_sid=ae9488&_nc_ohc=jqPBa6NSqrUAX9E19ED&_nc_ht=scontent.ftun4-2.fna&oh=03_AdQ5Q3n_THaFLkcqN3pw5s7ui3F2PPzRUKyt7PtjQSr-9A&oe=64B3690D" style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt="Image 1" />
+                    </div>
+                </Carousel>
+                {/* <AwesomeSlider>
                     <div data-src="https://scontent.ftun4-2.fna.fbcdn.net/v/t1.15752-9/256779859_213304307500755_4805841473852462713_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=101&ccb=1-7&_nc_sid=ae9488&_nc_ohc=PwGwkKQlBzQAX8PcMS1&_nc_ht=scontent.ftun4-2.fna&oh=03_AdTey-4_3YWe_8inhAV6glbteLoxJc0jlW09YIkwEVziqw&oe=64B37421" />
                     <div data-src="https://scontent.ftun4-2.fna.fbcdn.net/v/t1.15752-9/354671672_250588797603709_4163798981858942408_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=111&ccb=1-7&_nc_sid=ae9488&_nc_ohc=jqPBa6NSqrUAX9E19ED&_nc_ht=scontent.ftun4-2.fna&oh=03_AdQ5Q3n_THaFLkcqN3pw5s7ui3F2PPzRUKyt7PtjQSr-9A&oe=64B3690D" />
                     <div data-src="https://scontent.ftun1-2.fna.fbcdn.net/v/t1.15752-9/311493749_678030923502945_1668701050765048863_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_ohc=Oeo2H_gcMncAX8n4OXk&_nc_ht=scontent.ftun1-2.fna&oh=03_AdRaMTNpFTtGFPR9ByKR8ujEEhcoL4bukAbCGCKHF7dHKQ&oe=64B40132"></div>
                     <div data-src="https://scontent.ftun1-2.fna.fbcdn.net/v/t1.15752-9/354387420_232395069575148_8789793081750067042_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=101&ccb=1-7&_nc_sid=ae9488&_nc_ohc=sHSiHAeLXhcAX-wpegC&_nc_ht=scontent.ftun1-2.fna&oh=03_AdT5NRapFqXjzrQo2FUtLXIaLmvOP2Ags0UdkeQbE54QmA&oe=64B405D4"></div>
-                </AwesomeSlider>
+                </AwesomeSlider> */}
             </section>
 
             <section className="banner spad">
@@ -149,27 +171,45 @@ export default function Home() {
 
                                 </div>
                                 <div className="banner__item__text">
-                                    <h2 style={{background:"#EDF6BD"}}>{HomeTranslations[language]["Nouvel arrivage 2023"]}</h2>
-                                    <Link to={routes.SHOP} style={{background:"#EDF6BD"}}>
+                                    <h2 style={{ background: "#EDF6BD" }}>{HomeTranslations[language]["Nouvel arrivage 2023"]}</h2>
+                                    <Link to={routes.SHOP} style={{ background: "#EDF6BD" }}>
                                         {HomeTranslations[language]["Achetez maintenant"]}
                                     </Link>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-6" style={{paddingBottom:"0px", marginBottom:"0px"}}>
+                        <div className="col-lg-6" style={{ paddingBottom: "0px", marginBottom: "0px" }}>
                             <div className="banner__item">
                                 <div className="banner__item__pic">
-                                    <img src="https://scontent.ftun14-1.fna.fbcdn.net/v/t1.15752-9/355388025_941481710445745_8559209456739488357_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=Bh0ugXAaVygAX_WvDvd&_nc_ht=scontent.ftun14-1.fna&oh=03_AdSb6Slx_ATTI5qSD9VZbMjfW7KOp9s_4piPuIDq7ngx2A&oe=64B72841" alt="" />
+                                <Carousel autoPlay={true} showArrows={true} interval={2000}>
+                    <div>
+                    <img src="https://scontent.ftun2-2.fna.fbcdn.net/v/t1.15752-9/355764545_588987246682425_2385581986172350581_n.jpg?_nc_cat=111&cb=99be929b-59f725be&ccb=1-7&_nc_sid=ae9488&_nc_ohc=oRmtSTxmDzMAX9MZ8mK&_nc_ht=scontent.ftun2-2.fna&oh=03_AdSgnkEcCTQB02gKe_KhkzbeoE9XLELoE34H_3WhSpynkw&oe=64BDE29E" style={{ width: '300px', height: '100%', maxHeight: '354px', objectFit: 'cover' }} alt="Image 1" />
+                    </div>
+                    <div>
+                        <img src="https://scontent.ftun2-2.fna.fbcdn.net/v/t1.15752-9/356279976_1323588698514334_5004331889787099663_n.jpg?_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=ae9488&_nc_ohc=NwZBEnkOSnEAX_hvi9u&_nc_ht=scontent.ftun2-2.fna&oh=03_AdSrw8LJo711cAXfqdh4T3WkUcNt85KmAoKIfuDTRkUAxw&oe=64BDD4D3" style={{ width: '300px', maxHeight: '354px', objectFit: 'cover' }} alt="Image 1" />
+                    </div>
+                    <div>
+                        <img src="https://scontent.ftun2-2.fna.fbcdn.net/v/t1.15752-9/355958397_626953279392414_2392151218209588322_n.jpg?_nc_cat=110&cb=99be929b-59f725be&ccb=1-7&_nc_sid=ae9488&_nc_ohc=lsU_IKK4EqAAX91QEQg&_nc_oc=AQn4k3Ze9_BPKYHNBKeqs8EeNKFZe61OTbRYwBgeYzNdgins2VeL57KYFnIetzdODIM&_nc_ht=scontent.ftun2-2.fna&oh=03_AdTU5zHtFdeARfFZnyddKyIOAD3qZkklIzIOmijKCPckow&oe=64BDC52E" style={{ width: '300px', maxHeight: '354px', objectFit: 'cover' }} alt="Image 1" />
+                    </div>
+                </Carousel>
+                                {/* <AwesomeSlider>
+                    <div data-src="https://scontent.ftun14-1.fna.fbcdn.net/v/t1.15752-9/355388025_941481710445745_8559209456739488357_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=Bh0ugXAaVygAX_WvDvd&_nc_ht=scontent.ftun14-1.fna&oh=03_AdSb6Slx_ATTI5qSD9VZbMjfW7KOp9s_4piPuIDq7ngx2A&oe=64B72841" />
+                    <div data-src="https://scontent.ftun2-2.fna.fbcdn.net/v/t1.15752-9/355764545_588987246682425_2385581986172350581_n.jpg?_nc_cat=111&cb=99be929b-59f725be&ccb=1-7&_nc_sid=ae9488&_nc_ohc=oRmtSTxmDzMAX9MZ8mK&_nc_ht=scontent.ftun2-2.fna&oh=03_AdSgnkEcCTQB02gKe_KhkzbeoE9XLELoE34H_3WhSpynkw&oe=64BDE29E" />
+                    <div data-src="https://scontent.ftun2-2.fna.fbcdn.net/v/t1.15752-9/356279976_1323588698514334_5004331889787099663_n.jpg?_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=ae9488&_nc_ohc=NwZBEnkOSnEAX_hvi9u&_nc_ht=scontent.ftun2-2.fna&oh=03_AdSrw8LJo711cAXfqdh4T3WkUcNt85KmAoKIfuDTRkUAxw&oe=64BDD4D3"></div>
+                    <div data-src="https://scontent.ftun2-2.fna.fbcdn.net/v/t1.15752-9/355958397_626953279392414_2392151218209588322_n.jpg?_nc_cat=110&cb=99be929b-59f725be&ccb=1-7&_nc_sid=ae9488&_nc_ohc=lsU_IKK4EqAAX91QEQg&_nc_oc=AQn4k3Ze9_BPKYHNBKeqs8EeNKFZe61OTbRYwBgeYzNdgins2VeL57KYFnIetzdODIM&_nc_ht=scontent.ftun2-2.fna&oh=03_AdTU5zHtFdeARfFZnyddKyIOAD3qZkklIzIOmijKCPckow&oe=64BDC52E"></div>
+                    <div data-src="https://scontent.ftun2-2.fna.fbcdn.net/v/t1.15752-9/355806407_997859074723154_1423734920648509889_n.jpg?_nc_cat=106&cb=99be929b-59f725be&ccb=1-7&_nc_sid=ae9488&_nc_ohc=_M1zcNQPM9oAX-GlLzL&_nc_ht=scontent.ftun2-2.fna&oh=03_AdRcC7gErA9V_u921_FzltLd0_VRf6OiPjU_ySaKqKZQXw&oe=64BDCC02"></div>
+                </AwesomeSlider>
+                 */}
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-6">
-                            <div className="banner__item__text" style={{paddingBottom:"40px"}}>
-                                    <h2 style={{background:"#EDF6BD"}}>{HomeTranslations[language]["Ovins & Brebis"]}</h2>
-                                    <Link to={routes.SHOP} style={{background:"#EDF6BD"}}>
-                                        {HomeTranslations[language]["Consultez Notre Shop"]}
-                                    </Link>
-                                </div>
+                            <div className="banner__item__text" style={{ paddingBottom: "40px" }}>
+                                <h2 style={{ background: "#EDF6BD" }}>{HomeTranslations[language]["Ovins & Brebis"]}</h2>
+                                <Link to={routes.SHOP} style={{ background: "#EDF6BD" }}>
+                                    {HomeTranslations[language]["Consultez Notre Shop"]}
+                                </Link>
+                            </div>
                         </div>
                         <div className="col-lg-7 offset-lg-4">
                             <div className="banner__item">
@@ -177,8 +217,8 @@ export default function Home() {
                                     <img src="https://scontent.ftun14-1.fna.fbcdn.net/v/t1.15752-9/355385132_931223464829885_5643326160151289013_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=ae9488&_nc_ohc=n9piaYL0ddoAX8B6zA1&_nc_ht=scontent.ftun14-1.fna&oh=03_AdQ72f0NROrlKPhvg4lESJh9NzPBivuwUSD_-lLFRbr1DA&oe=64B7296B" alt="" />
                                 </div>
                                 <div className="banner__item__text">
-                                    <h2 style={{background:"#EDF6BD"}}>{HomeTranslations[language]["Poulailler & Agnelles"]}</h2>
-                                    <Link to={routes.ABOUTUS} style={{background:"#EDF6BD"}}>
+                                    <h2 style={{ background: "#EDF6BD" }}>{HomeTranslations[language]["Poulailler & Agnelles"]}</h2>
+                                    <Link to={routes.ABOUTUS} style={{ background: "#EDF6BD" }}>
                                         {HomeTranslations[language]["Contactez Nous"]}
                                     </Link>
                                 </div>
@@ -190,32 +230,32 @@ export default function Home() {
 
             <section className="product spad">
                 <div className="container">
-                <SimpleDialog
-                    selectedValue={selectedValue}
-                    open={isOpen}
-                    onClose={handleClickClose}
+                    <SimpleDialog
+                        selectedValue={selectedValue}
+                        open={isOpen}
+                        onClose={handleClickClose}
                     />
-                <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }}>
-                    <Toolbar>
-                        <SpeedDial
-                            ariaLabel="Settings"
-                            sx={{ position: 'absolute', bottom: 16, right: 16 }}
-                            icon={<SettingsIcon />}
-                            onClose={handleClose}
-                            onOpen={handleOpen}
-                            open={open}
-                        >
-                            {actions.map((action) => (
-                                <SpeedDialAction
-                                    key={action.key}
-                                    icon={action.icon}
-                                    tooltipTitle={action.name}
-                                    onClick={() => handleAction(action.key)}
-                                />
-                            ))}
-                        </SpeedDial>
-                    </Toolbar>
-                </AppBar>
+                    <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }}>
+                        <Toolbar>
+                            <SpeedDial
+                                ariaLabel="Settings"
+                                sx={{ position: 'absolute', bottom: 16, right: 16 }}
+                                icon={<SettingsIcon />}
+                                onClose={handleClose}
+                                onOpen={handleOpen}
+                                open={open}
+                            >
+                                {actions.map((action) => (
+                                    <SpeedDialAction
+                                        key={action.key}
+                                        icon={action.icon}
+                                        tooltipTitle={action.name}
+                                        onClick={() => handleAction(action.key)}
+                                    />
+                                ))}
+                            </SpeedDial>
+                        </Toolbar>
+                    </AppBar>
                     <div className="row">
                         <MyContext.Provider value={{ activeFilter, handleFilterClick }}>
                             <ActiveTabs
